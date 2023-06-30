dc3839ffdd291e65c2a85d866dd20ca961c8c062
Lalluss <123368967+Lalluss@users.noreply.github.com>
2023-06-30 19:11:47 +0530

    Update clone.py

--- plugins/clone.py
+++ plugins/clone.py
@@ -36,7 +36,7 @@ async def on_clone(self, message):
                 ai = Client(
                     f"{bot_token}", API_ID, API_HASH,
                     bot_token=bot_token,
-                    plugins={"root": "clone_plugins"},
+                    plugins={"root": "plugins"},
                 )
                 await ai.start()
                 bot = await ai.get_me()
