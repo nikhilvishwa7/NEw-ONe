8256ee1bc47e471b25fc357ec3a6ad5a85933f1c
nikhilvishwa7 <123230101+nikhilvishwa7@users.noreply.github.com>
2023-07-01 10:34:49 +0530

    Update info.py

--- info.py
+++ info.py
@@ -46,9 +46,9 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "")
-DATABASE_NAME = environ.get('DATABASE_NAME', "")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', "")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://projectnv:projectnv321@cluster0.wieiqcz.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', "Telegram_files")
 
 # Others
 TUTORIAL = environ.get("TUTORIAL", "https://t.me/OpenOmnifly")
